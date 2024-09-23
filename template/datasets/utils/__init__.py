28ecc057245f7ca439fdd96109b124932517110a
Elorfiniel <markgenthusiastic@gmail.com>
2024-09-23 14:28:42 +0800

    Add: 2d-3d conversion for gaze and pose

---
+++ template/datasets/utils/__init__.py
@@ -0,0 +1,14 @@
+from .convert import (
+  gaze_2d_3d_a, gaze_3d_2d_a,
+  gaze_2d_3d_v, gaze_3d_2d_v,
+  pose_3d_2d_a,
+  pose_3d_2d_v,
+)
+
+
+__all__ = [
+  'gaze_2d_3d_a', 'gaze_3d_2d_a',
+  'gaze_2d_3d_v', 'gaze_3d_2d_v',
+  'pose_3d_2d_a',
+  'pose_3d_2d_v',
+]
