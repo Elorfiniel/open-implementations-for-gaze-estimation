e8719b030f2736ea398e6e869923abb3a016858f
Elorfiniel <markgenthusiastic@gmail.com>
2024-09-24 17:03:17 +0800

    Add: schedule configs for sgd training for 20 epochs, mslr with warmup

---
+++ configs/lenet/lenet-2xb64-warmup-mslr-20e.py
@@ -0,0 +1,17 @@
+_base_ = [
+  '../_base_/datasets/mpiigaze.py',
+  '../_base_/models/lenet.py',
+  '../_base_/schedules/schedule_20e-sgd-warmup-mslr.py',
+  '../_base_/default_runtime.py',
+]
+
+custom_hooks = [
+  dict(
+    type='CheckpointHook',
+    interval=1,
+    by_epoch=True,
+    save_best='mae',
+    rule='less',
+    save_last=False,
+  ),
+]
