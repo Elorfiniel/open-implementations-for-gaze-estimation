1f4b61edcd34dc788f0a0dbbbaa1cc27e10c7598
Elorfiniel <markgenthusiastic@gmail.com>
2025-05-19 20:26:20 +0800

    Add preprocessing code from itracker paper

--- scripts/preprocess/gazecapture-itracker.py
+++ scripts/preprocess/gazecapture-itracker.py
@@ -1,7 +1,5 @@
-# This script follows the pipeline from the `gaze-point-estimation-2023`
-# project, rather than that from the "Eye Tracking for Everyone" paper
+# This script follows the pipeline from the "Eye Tracking for Everyone" paper
 
-from opengaze.utils import FaceLandmarks, FaceAlignment
 from opengaze.runtime.scripts import ScriptEnv
 from opengaze.runtime.log import runtime_logger
 from opengaze.runtime.parallel import FunctionalTask, run_parallel
@@ -27,19 +25,29 @@ def save_json_data(json_data: dict, json_file: str):
   with open(json_file, 'w') as file:
     json.dump(json_data, file, indent=2)
 
-def load_validation(json_file: str):
-  validation = load_json_data(json_file)['IsValid']
-  return np.asarray(validation, dtype=bool)
-
 def load_gc_annot_subject(subject_folder):
   metadata = load_json_data(osp.join(subject_folder, 'info.json'))
 
   image_names = load_json_data(osp.join(subject_folder, 'frames.json'))
   gaze_labels = load_json_data(osp.join(subject_folder, 'dotInfo.json'))
 
-  face_valid = load_validation(osp.join(subject_folder, 'appleFace.json'))
-  reye_valid = load_validation(osp.join(subject_folder, 'appleRightEye.json'))
-  leye_valid = load_validation(osp.join(subject_folder, 'appleLeftEye.json'))
+  apple_face = load_json_data(osp.join(subject_folder, 'appleFace.json'))
+  apple_reye = load_json_data(osp.join(subject_folder, 'appleRightEye.json'))
+  apple_leye = load_json_data(osp.join(subject_folder, 'appleLeftEye.json'))
+  apple_grid = load_json_data(osp.join(subject_folder, 'faceGrid.json'))
+
+  face_valid = np.asarray(apple_face['IsValid'], dtype=bool)
+  reye_valid = np.asarray(apple_reye['IsValid'], dtype=bool)
+  leye_valid = np.asarray(apple_leye['IsValid'], dtype=bool)
+
+  fetch_bbox = lambda d: np.stack([d['X'], d['Y'], d['W'], d['H']], axis=1).astype(int)
+
+  face_bbox = fetch_bbox(apple_face) + np.array([-1, -1, 1, 1])
+  reye_bbox = fetch_bbox(apple_reye) + np.array([0, -1, 0, 0])
+  reye_bbox[:, :2] += face_bbox[:, :2]
+  leye_bbox = fetch_bbox(apple_leye) + np.array([0, -1, 0, 0])
+  leye_bbox[:, :2] += face_bbox[:, :2]
+  grid_bbox = fetch_bbox(apple_grid)
 
   label_file = np.array([
     z for z in zip(image_names, gaze_labels['XCam'], gaze_labels['YCam'])
@@ -47,21 +55,31 @@ def load_gc_annot_subject(subject_folder):
   image_valid = face_valid & reye_valid & leye_valid
 
   valid_labels = label_file[image_valid]
+  valid_bboxes = dict(
+    face=face_bbox[image_valid],
+    reye=reye_bbox[image_valid],
+    leye=leye_bbox[image_valid],
+    grid=grid_bbox[image_valid],
+  )
 
-  return metadata, valid_labels
+  return metadata, valid_labels, valid_bboxes
 
-def rotate_vector(vector: np.ndarray, angle: float):
-  radian = np.deg2rad(angle, dtype=np.float32)
+def image_crop(image: np.ndarray, bbox: np.ndarray):
+  a_src = np.maximum(bbox[:2], 0)
+  b_src = np.minimum(bbox[:2] + bbox[2:], (image.shape[1], image.shape[0]))
 
-  cos, sin = np.cos(radian), np.sin(radian)
-  mat = np.array([[cos, -sin], [sin, cos]])
+  a_dst = a_src - bbox[:2]
+  b_dst = a_dst + b_src - a_src
 
-  return np.dot(mat, vector)
+  crop = np.zeros((bbox[3], bbox[2], 3), dtype=np.uint8)
+  crop[a_dst[1]:b_dst[1], a_dst[0]:b_dst[0]] = image[a_src[1]:b_src[1], a_src[0]:b_src[0]]
+
+  return cv2.resize(crop, (224, 224), interpolation=cv2.INTER_CUBIC)
 
 def process_subject(subjects_folder, subject, opt_folder):
   # Load annotations for current subject
   subject_folder = osp.join(subjects_folder, subject)
-  metadata, valid_labels = load_gc_annot_subject(subject_folder)
+  metadata, valid_labels, valid_bboxes = load_gc_annot_subject(subject_folder)
 
   # Create output folder for current subject
   subject_opt_folder = osp.join(opt_folder, subject)
@@ -74,15 +92,10 @@ def process_subject(subjects_folder, subject, opt_folder):
     dtype=np.float32, chunks=(1, 2),
     maxshape=(len(valid_labels), 2),
   )
-  bbox = hdf_file.create_dataset(
-    'bbox', shape=(len(valid_labels), 12),
-    dtype=np.float32, chunks=(1, 12),
-    maxshape=(len(valid_labels), 12),
-  )
-  ldmk = hdf_file.create_dataset(
-    'ldmk', shape=(len(valid_labels), 478, 2),
-    dtype=np.float32, chunks=(1, 478, 2),
-    maxshape=(len(valid_labels), 478, 2),
+  grid = hdf_file.create_dataset(
+    'grid', shape=(len(valid_labels), 4),
+    dtype=int, chunks=(1, 4),
+    maxshape=(len(valid_labels), 4),
   )
 
   # Create image patch folders
@@ -94,51 +107,39 @@ def process_subject(subjects_folder, subject, opt_folder):
   os.makedirs(leye_folder, exist_ok=True)
 
   # Process gaze data for current subject
-  alignment = FaceAlignment(width_expand=1.6, hw_ratio=1.0)
   n_samples = 0 # Number of final samples
 
-  with FaceLandmarks(p_detection=0.6, p_presence=0.6) as landmarker:
-    for idx in range(len(valid_labels)):
-      sample = valid_labels[idx]  # Numpy structured array
+  for idx in range(len(valid_labels)):
+    sample = valid_labels[idx]  # Numpy structured array
 
-      img = cv2.imread(
-        osp.join(subject_folder, 'frames', sample['image']),
-        flags=cv2.IMREAD_UNCHANGED,
-      )
-      pog = np.array(sample[['gx_cm', 'gy_cm']].tolist(), dtype=np.float32)
+    img = cv2.imread(
+      osp.join(subject_folder, 'frames', sample['image']),
+      flags=cv2.IMREAD_UNCHANGED,
+    )
+    pog = np.array(sample[['gx_cm', 'gy_cm']].tolist(), dtype=np.float32)
 
-      landmarks = landmarker.process(img, bgr2rgb=True)
-      if landmarks is None: continue
-      align_dict = alignment.align(img, landmarks)
+    cv2.imwrite(
+      osp.join(face_folder, sample['image']),
+      image_crop(img, valid_bboxes['face'][idx]),
+      [cv2.IMWRITE_JPEG_QUALITY, 100],
+    )
+    cv2.imwrite(
+      osp.join(reye_folder, sample['image']),
+      image_crop(img, valid_bboxes['reye'][idx]),
+      [cv2.IMWRITE_JPEG_QUALITY, 100],
+    )
+    cv2.imwrite(
+      osp.join(leye_folder, sample['image']),
+      image_crop(img, valid_bboxes['leye'][idx]),
+      [cv2.IMWRITE_JPEG_QUALITY, 100],
+    )
+    gaze[idx] = pog
+    grid[idx] = valid_bboxes['grid'][idx]
 
-      cv2.imwrite(
-        osp.join(face_folder, sample['image']),
-        align_dict['face_crop'],
-        [cv2.IMWRITE_JPEG_QUALITY, 100],
-      )
-      cv2.imwrite(
-        osp.join(reye_folder, sample['image']),
-        align_dict['reye_crop'],
-        [cv2.IMWRITE_JPEG_QUALITY, 100],
-      )
-      cv2.imwrite(
-        osp.join(leye_folder, sample['image']),
-        align_dict['leye_crop'],
-        [cv2.IMWRITE_JPEG_QUALITY, 100],
-      )
-      gaze[idx] = rotate_vector(pog, -align_dict['theta'])
-      bbox[idx] = np.concatenate([
-        align_dict['face_bbox'],
-        align_dict['reye_bbox'],
-        align_dict['leye_bbox'],
-      ], axis=0)
-      ldmk[idx] = align_dict['ldmks']
-
-      n_samples = n_samples + 1
+    n_samples = n_samples + 1
 
   gaze.resize(n_samples, axis=0)
-  bbox.resize(n_samples, axis=0)
-  ldmk.resize(n_samples, axis=0)
+  grid.resize(n_samples, axis=0)
 
   # Close hdf file
   hdf_file.close()
