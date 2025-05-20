6f59e30f0bd53ab83db4068418044e597bc21103
Elorfiniel <markgenthusiastic@gmail.com>
2025-05-20 09:53:57 +0800

    Make face grid from face bbox for iTracker model

--- opengaze/engine/transform/__init__.py
+++ opengaze/engine/transform/__init__.py
@@ -1,2 +1,11 @@
+from torchvision import transforms as tvt
+
+from opengaze.registry import TRANSFORMS
+
 from .base import BaseTransform
-from .torch import *
+from .gaze_2d import ITrackerMakeGrid
+
+
+TRANSFORMS.register_module(name='Grayscale', module=tvt.Grayscale)
+TRANSFORMS.register_module(name='Normalize', module=tvt.Normalize)
+TRANSFORMS.register_module(name='ToTensor', module=tvt.ToTensor)
