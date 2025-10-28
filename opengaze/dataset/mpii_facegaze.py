Fix incorrect pose calculation, update data organization

opengaze/dataset/mpii_facegaze.py
@@ -85,7 +85,7 @@ class _MPIIFaceGazePP(Dataset):
     } # Load all data except images in memory
 
     for dd in self.dates:
-      with h5py.File(osp.join(root, f'{dd}.h5'), 'r', swmr=True) as hdf_file:
+      with h5py.File(osp.join(root, dd, f'annot.h5'), 'r', swmr=True) as hdf_file:
         for key in self.data.keys():
           value = np.array(hdf_file[key])
           self.data[key].append(value)
@@ -115,7 +115,7 @@ class _MPIIFaceGazePP(Dataset):
       sample_idx = idx - self.cumulative_sizes[date_idx - 1]
 
     face = self._load_crop(
-      osp.join(self.root, self.dates[date_idx], f'{sample_idx:04d}.jpg'),
+      osp.join(self.root, self.dates[date_idx], 'face', f'{sample_idx:04d}.jpg'),
     )
     gaze = self.data['face-gaze'][date_idx][sample_idx]
     pose = self.data['face-pose'][date_idx][sample_idx]

opengaze/dataset/mpii_gaze.py
@@ -105,7 +105,7 @@ class _MPIIGazePP(Dataset):
         lines = [line.strip() for line in fp]
         l_eval, r_eval = self._parse_eval_lines(lines)
 
-    hdf_filenames = sorted(os.listdir(root))
+    dates = sorted(os.listdir(root))
 
     self.data = {
       'leye-img': [],
@@ -116,10 +116,8 @@ class _MPIIGazePP(Dataset):
       'reye-pose': [],
     } # Load all data in memory (~1GB)
 
-    for hdf_filename in hdf_filenames:
-      with h5py.File(osp.join(root, hdf_filename), 'r', swmr=True) as hdf_file:
-        dd = osp.splitext(hdf_filename)[0]
-
+    for dd in dates:
+      with h5py.File(osp.join(root, dd, 'annot.h5'), 'r', swmr=True) as hdf_file:
         for key in self.data.keys():
           value = np.array(hdf_file[key])
           if eval_subset:

prepare/mpii-facegaze.py
@@ -55,7 +55,7 @@ class Camera2Normal:
 
     # Revisiting Data Normalization: discard the scaling component `S_x`
     gaze = self._unit_vector(np.dot(R2, tgt - look_at))
-    pose = self._unit_vector(cv2.Rodrigues(np.dot(R2, R1))[0].reshape((3, )))
+    pose = cv2.Rodrigues(np.dot(R2, R1))[0].reshape((3, ))
 
     return warp, gaze, pose
 
@@ -117,6 +117,15 @@ def load_mpii_annot_pp_dd(date_folder, **kwargs):
 
   return label_file
 
+def calculate_face_center(landmarks_3d: np.ndarray):
+  # Use convention of ETH-XGaze data normalization
+
+  eyes_center = np.mean(landmarks_3d[:, 0:4], axis=1)
+  mouth_center = np.mean(landmarks_3d[:, 4:6], axis=1)
+  face_center = 0.5 * (eyes_center + mouth_center)
+
+  return face_center
+
 def process_pp_dd(persons_folder, pp, dd, opt_folder):
   # Load calibration data, such as camera matrix, screen size, etc
   calib_folder = osp.join(persons_folder, pp, 'Calibration')
@@ -128,12 +137,12 @@ def process_pp_dd(persons_folder, pp, dd, opt_folder):
   date_folder = osp.join(persons_folder, pp, dd)
   label_file = load_mpii_annot_pp_dd(date_folder, pp=pp, dd=dd)
 
-  # Create output folder for current person
-  person_opt_folder = osp.join(opt_folder, pp)
-  os.makedirs(person_opt_folder, exist_ok=True)
+  # Create output folder for current person and date
+  pp_dd_opt_folder = osp.join(opt_folder, pp, dd)
+  os.makedirs(pp_dd_opt_folder, exist_ok=True)
 
   # Create hdf datasets
-  hdf_file = h5py.File(osp.join(person_opt_folder, f'{dd}.h5'), 'w')
+  hdf_file = h5py.File(osp.join(pp_dd_opt_folder, f'annot.h5'), 'w')
   face_gaze = hdf_file.create_dataset(
     'face-gaze', shape=(len(label_file), 3),
     dtype=np.float32, chunks=(1, 3),
@@ -143,7 +152,7 @@ def process_pp_dd(persons_folder, pp, dd, opt_folder):
     dtype=np.float32, chunks=(1, 3),
   )
   # Create face patch folders
-  face_folder = osp.join(person_opt_folder, f'{dd}')
+  face_folder = osp.join(pp_dd_opt_folder, 'face')
   os.makedirs(face_folder, exist_ok=True)
 
   # Process gaze data for current person and date
@@ -171,12 +180,12 @@ def process_pp_dd(persons_folder, pp, dd, opt_folder):
     hr, ht = pose_estim.estimate(landmarks_2d)
     hR = cv2.Rodrigues(hr)[0]
     landmarks_3d = np.dot(hR, pose_estim.face_model.T) + ht
-    fe = np.average(landmarks_3d, axis=1)
+    fe = calculate_face_center(landmarks_3d)
 
     img_f, gaze_f, pose_f = cam2nor.normalize_data(fe, hR, img, tgt)
     cv2.imwrite(
       osp.join(face_folder, f'{idx:04d}.jpg'),
-      img_f,
+      cv2.resize(img_f, (224, 224), interpolation=cv2.INTER_CUBIC),
       [cv2.IMWRITE_JPEG_QUALITY, 100],
     )
     face_gaze[idx] = gaze_f; face_pose[idx] = pose_f

prepare/mpii-gaze.py
@@ -55,7 +55,7 @@ class Camera2Normal:
 
     # Revisiting Data Normalization: discard the scaling component `S_x`
     gaze = self._unit_vector(np.dot(R2, tgt - look_at))
-    pose = self._unit_vector(cv2.Rodrigues(np.dot(R2, R1))[0].reshape((3, )))
+    pose = cv2.Rodrigues(np.dot(R2, R1))[0].reshape((3, ))
 
     return warp, gaze, pose
 
@@ -122,12 +122,12 @@ def process_pp_dd(persons_folder, pp, dd, opt_folder):
   date_folder = osp.join(persons_folder, pp, dd)
   label_file = load_mpii_annot_pp_dd(date_folder)
 
-  # Create output folder for current person
-  person_opt_folder = osp.join(opt_folder, pp)
-  os.makedirs(person_opt_folder, exist_ok=True)
+  # Create output folder for current person and date
+  pp_dd_opt_folder = osp.join(opt_folder, pp, dd)
+  os.makedirs(pp_dd_opt_folder, exist_ok=True)
 
   # Create hdf datasets
-  hdf_file = h5py.File(osp.join(person_opt_folder, f'{dd}.h5'), 'w')
+  hdf_file = h5py.File(osp.join(pp_dd_opt_folder, f'annot.h5'), 'w')
   leye_img = hdf_file.create_dataset(
     'leye-img', shape=(len(label_file), 36, 60),
     dtype=np.uint8, chunks=(1, 36, 60),